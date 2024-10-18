7273bf31dbbcd0274e2802334d2bfbadaf1685ed
peter <mercy2green@gmail.com>
2024-10-18 12:34:19 +0800

    1

--- vln_sim2real.sh
+++ vln_sim2real.sh
@@ -1,40 +1,17 @@
-# #!
-
-
-conda create -n vln_310 anaconda python=3.10 cmake=3.14.0
-conda activate vln_310
-
-# conda env update vln_sim2real -f vln_sim2real.yml
+#!
 
 pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple # If you are in Mainland.
 
-##### Install Pytorch according to your own setup #####
-# For example, if you have a GPU with CUDA 11.8 (We tested it Pytorch 2.0.1)
-conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.8 -c pytorch -c nvidia
-conda install -c conda-forge cudatoolkit-dev
-
-# Install the required libraries
-pip install tyro open_clip_torch wandb h5py openai hydra-core distinctipy
-
-# for yolo
-pip install ultralytics
-
-# Install the Faiss library (CPU version should be fine)
-conda install -c pytorch faiss-cpu=1.7.4 mkl=2021 blas=1.0=mkl
-
-# Install Pytorch3D (https://github.com/facebookresearch/pytorch3d/blob/main/INSTALL.md)
-# conda install pytorch3d -c pytorch3d # This detects a conflict. You can use the command below, maybe with a different version
-conda install https://anaconda.org/pytorch3d/pytorch3d/0.7.4/download/linux-64/pytorch3d-0.7.4-py310_cu118_pyt201.tar.bz2
-
-
 pip install openai-clip trimesh
 
-# git clone https://github.com/Mercy2Green/Chamferdist_c17.git
+pip install torch==2.0.1+cu118 torchvision==0.15.2+cu118 --index-url https://download.pytorch.org/whl/cu118
+
+git clone https://github.com/Mercy2Green/Chamferdist_c17.git
 cd Chamferdist_c17
 pip install .
 cd ..
 
-# git clone https://github.com/gradslam/gradslam.git
+git clone https://github.com/gradslam/gradslam.git
 cd gradslam
 git checkout conceptfusion
 pip install .
